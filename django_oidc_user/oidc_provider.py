1a3f2eaa8bfadfa368a32abcc1a0173ad521fafb
Monte Hellawell <hellawell@monte.me.uk>
2017-10-19 17:23:14 +0100

    Add django-oidc-provider userinfo support

---
+++ django_oidc_user/oidc_provider.py
@@ -0,0 +1,16 @@
+def userinfo(claims, user):
+	claims['name'] = '{0} {1}'.format(user.first_name, user.last_name)
+	claims['given_name'] = user.first_name
+	claims['family_name'] = user.last_name
+	claims['preferred_username'] = user.username
+	claims['website'] = user.website
+	claims['zoneinfo'] = user.zoneinfo
+	cliams['locale'] = user.locale
+
+	claims['email'] = user.email
+	claims['email_verified'] = user.email_verified
+
+	claims['phone_number'] = user.phone
+	claims['phone_number_verified'] = user.phone_verified
+
+	return claims
