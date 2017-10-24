20b42ae1745a5f5bc1f42925993f88468b7e211d
Monte Hellawell <hellawell@monte.me.uk>
2017-10-24 20:22:06 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
 	name = 'django-oidc-user',
 	packages = find_packages(),
-	version = '0.1.1',
+	version = '0.2.0',
 	description = 'Creates an OpenID Connect compliant User model.',
 	author = 'Monte Hellawell',
 	author_email = 'hellawell@monte.me.uk',
 	url = 'https://github.com/montudor/django-oidc-user',
-	download_url = 'https://github.com/montudor/django-oidc-user/archive/v0.1.1.tar.gz',
+	download_url = 'https://github.com/montudor/django-oidc-user/archive/v0.2.0.tar.gz',
 	keywords = ['django'],
 	classifiers = [],
 )
