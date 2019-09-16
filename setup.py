d8880e39f9883792e01d0cc13e0f8152e08b2c00
Monte Hellawell <monte@montudor.com>
2019-09-16 10:03:09 +0100

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import os
 from setuptools import (
-	find_packages,
-	setup,
+    find_packages,
+    setup,
 )
 
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-	name = 'django-oidc-user',
-	packages = find_packages(),
-	version = '0.3.3',
-	description = 'Creates an OpenID Connect compliant User model.',
-	author = 'Monte Hellawell',
-	author_email = 'hellawell@monte.me.uk',
-	url = 'https://github.com/montudor/django-oidc-user',
-	download_url = 'https://github.com/montudor/django-oidc-user/archive/v0.3.3.tar.gz',
-	keywords = ['django'],
-	classifiers = [],
+    name='django-oidc-user',
+    packages=find_packages(),
+    version='0.3.3',
+    description='Creates an OpenID Connect compliant User model.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Monte Hellawell',
+    author_email='monte@montudor.com',
+    url='https://github.com/montudor/django-oidc-user',
+    download_url='https://github.com/montudor/django-oidc-user/archive/v0.3.3.tar.gz',
+    keywords=['django'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
 )
