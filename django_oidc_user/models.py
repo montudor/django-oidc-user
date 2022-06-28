69c6efa632a016b8343e2b05d6f982a0636e2662
Monte Hellawell <monte@montudor.com>
2022-06-28 09:26:15 +0100

    Fix issue with latest Django version

--- django_oidc_user/models.py
+++ django_oidc_user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core import validators
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import UserManager
